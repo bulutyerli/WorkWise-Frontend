0f9d308c5bfb7c555a038b0f08ec00178b20a05d
bulut yerli <bulut@buluts-Mac-mini.local>
2024-05-18 09:31:06 +0300

    fixed sign in page navigation on success

--- src/routes/app/_layout/_authenticated.tsx
+++ src/routes/app/_layout/_authenticated.tsx
@@ -1,13 +1,12 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/app/_layout/_authenticated')({
-  beforeLoad: async ({ context, location }) => {
+  beforeLoad: async ({ context }) => {
     const auth = context.auth.isAuthenticated;
 
     if (!auth) {
       throw redirect({
         to: '/app/auth/sign-in',
-        search: { redirect: location.href },
       });
     }
   },
