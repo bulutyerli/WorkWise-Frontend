be89bdbb05e9badabca617f12a0f1787b7c632a8
bulut yerli <bulut@buluts-Mac-mini.local>
2024-06-03 04:45:11 +0300

    fix containerElem any type

--- src/utils/useCenteredTree.ts
+++ src/utils/useCenteredTree.ts
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 
 export const useCenteredTree = () => {
   const [translate, setTranslate] = useState({ x: 0, y: 0 });
-  const containerRef = useCallback((containerElem) => {
+  const containerRef = useCallback((containerElem: HTMLElement | null) => {
     if (containerElem !== null) {
       const { width, height } = containerElem.getBoundingClientRect();
       setTranslate({ x: width / 2, y: height / 2 });
