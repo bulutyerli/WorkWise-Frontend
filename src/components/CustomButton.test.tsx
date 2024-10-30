9a86725982c789f97f2b21c3f6280abf3792efcb
bulut yerli <bulutyerli@gmail.com>
2024-10-30 20:08:38 +0300

    Add tests for CustomButton component

---
+++ src/components/CustomButton.test.tsx
@@ -0,0 +1,64 @@
+import { render } from '@testing-library/react';
+import CustomButton from './CustomButton';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+
+vi.mock('./LoadingSpinner', () => ({
+  default: () => <div>Spinner</div>,
+}));
+
+const mockOnClick = vi.fn();
+const icon = <svg>icon</svg>;
+const color = 'primary';
+const text = 'test';
+
+describe('CustomButton', () => {
+  it('should render correct text if loading or not', () => {
+    const { rerender } = render(
+      <CustomButton
+        onClick={mockOnClick}
+        icon={icon}
+        color={color}
+        text={text}
+        isLoading={false}
+      />
+    );
+
+    const button = screen.getByRole('button', {
+      name: /test/i,
+    });
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveClass('bg-orange-600 hover:bg-orange-500');
+    expect(screen.queryByText('Spinner')).not.toBeInTheDocument();
+
+    rerender(
+      <CustomButton
+        onClick={mockOnClick}
+        icon={icon}
+        color={color}
+        text={text}
+        isLoading={true}
+      />
+    );
+
+    expect(screen.queryByText(/test/i)).not.toBeInTheDocument();
+    expect(screen.getByText('Spinner')).toBeInTheDocument();
+  });
+
+  it('should call onClick function on click', async () => {
+    render(
+      <CustomButton
+        onClick={mockOnClick}
+        icon={icon}
+        color={color}
+        text={text}
+        isLoading={false}
+      />
+    );
+    const button = screen.getByRole('button', { name: /test/i });
+    const user = userEvent.setup();
+    await user.click(button);
+    expect(mockOnClick).toHaveBeenCalledOnce();
+  });
+});
